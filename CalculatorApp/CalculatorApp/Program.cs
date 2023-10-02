811e70008ff7a7286da758247a013235b21ae3a0
Emma <146089189+emma-veingartner@users.noreply.github.com>
2023-10-02 14:57:31 +0100

    Adding options for the calculator

--- CalculatorApp/CalculatorApp/Program.cs
+++ CalculatorApp/CalculatorApp/Program.cs
@@ -1,10 +1,43 @@
-﻿namespace CalculatorApp
+﻿using System;
+using System.Reflection.Emit;
+
+namespace CalculatorApp
 {
+    
     internal class Program
-    {
+    { 
+        
+
+        enum Menu { Sum = 1, Subtraction = 2, Division = 3, Multiplication = 4, Power = 5, Root = 6, Exit = 7 }
+       
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            bool choosedExit = false;
+            while (!choosedExit) // While the user DO NOT(!) choose Exit, show the Menu.
+            {
+                Console.WriteLine("Welcome to the Calculator App! Please, select one of the options:");
+                Console.WriteLine("1-Sum\n2-Subtraction\n3-Division\n4-Multiplication\n5-Power\n6-Root\n7-Exit");
+
+                /*
+                string optionsTXT = Console.ReadLine();
+                int optionsInt = int.Parse(optionsTXT);
+                Menu options = (Menu)optionsInt;
+                */
+
+                Menu options = (Menu)int.Parse(Console.ReadLine());
+
+                Console.WriteLine(options);
+
+                
+
+            }
+
+            
+            
+
+
+
         }
     }
 }
