60db67a28730d26c9548b447688cf579b426f0eb
Emma <146089189+emma-veingartner@users.noreply.github.com>
2023-10-09 12:36:04 +0100

    Constructors and Scope

--- CustomTypes/CustomTypes/Program.cs
+++ CustomTypes/CustomTypes/Program.cs
@@ -15,15 +15,26 @@ namespace CustomTypes
             public float price;
             public float weight;
             public string brand;
+
+            public Product (string name, float price, float weight, string brand)
+            {
+                this.name = name;
+                this.price = price;
+                this.weight = weight;
+                this.brand = brand;
+            }
         }
 
         static void Main(string[] args)
         {
-            Product ball = new Product();
-            ball.name = "Ronaldo's ball";
-            ball.price = 14f;
-            ball.weight = 1f;
-            ball.brand = "Adidas";
+            Product ball = new Product("Ronaldinho's ball", 12f, 1f, "Adidas");
+            Product bucket = new Product("Pink bucket", 8f, 2f, "Tupperware");
+
+            Console.WriteLine(ball.name);
+            Console.WriteLine(bucket.name);
+            Console.ReadLine();
+
+            
         }
     }
 }
