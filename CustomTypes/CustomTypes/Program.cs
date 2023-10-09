c0fa41c94d1973a806aadf1d5b07b4910ca2afda
Emma <146089189+emma-veingartner@users.noreply.github.com>
2023-10-09 12:26:22 +0100

    Structs

---
+++ CustomTypes/CustomTypes/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomTypes
+{
+    internal class Program
+    {
+
+        struct Product
+        {
+            public string name;
+            public float price;
+            public float weight;
+            public string brand;
+        }
+
+        static void Main(string[] args)
+        {
+            Product ball = new Product();
+            ball.name = "Ronaldo's ball";
+            ball.price = 14f;
+            ball.weight = 1f;
+            ball.brand = "Adidas";
+        }
+    }
+}
